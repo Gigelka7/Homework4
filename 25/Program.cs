5c491b137fca4e6ba2e453a8cde0a3fa26d47fc1
Kolesnik Anton <kolesnikas2006@gmail.com>
2022-11-20 18:30:28 +0500

    as

---
+++ 25/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 3;
+int b = 5;
+int Exponentiation(int a, int b)
+{
+    int c = 1;
+    for(int i=1; i <= b; i++)
+    {
+        c = c * a;
+    }
+    return c;
+}
+int d = Exponentiation(a, b);
+Console.WriteLine(d);
---
+++ 27/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 345;
+int b = 0;
+while (a > 0)
+ {
+ 
+    b = b + a % 10;
+    a = a /10 ;
+ 
+}
+Console.WriteLine(b);
